a8e67790c6fea74fd9857095f88dbd59f3d053df
sarca <zarypkanov270301@gmail.com>
2022-03-16 13:02:10 +0600

    Fix log error

--- Philosophy/urls_config.py
+++ Philosophy/urls_config.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPage.as_view(), name='MainPage'),
     path('login/', LogUser.as_view(), name='Login'),
-    path('accounts/profile/',LogUser.as_view(),name='Login'),
+    # path('accounts/profile/',LogUser.as_view(),name='Login'),
     path('register', RegUser.as_view(), name='Register'),
     path('add-article', AddArticle.as_view(), name='AddArticle'),
     path('search-resutl', SearchResultsView.as_view(), name='search_results'),
